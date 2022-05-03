716dc65ba4733c0aaaa3334f7f75d64ab614145b
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-05-03 15:35:58 -0300

    feat: add border

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
 var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
 
-$(document).ready(function() {
+$(document).ready(function () {
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 });
 
@@ -16,12 +17,12 @@ function atualizaTamanhoFrase() {
 }
 
 function inicializaContadores() {
-    campo.on("input", function(){
+    campo.on("input", function () {
         var conteudo = campo.val();
-    
-        var qtdPalavras = conteudo.split(/\S+/).length -1;
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
         $("#contador-palavras").text(qtdPalavras);
-    
+
         var qtdCaracteres = conteudo.length;
         $("#contador-caracteres").text(qtdCaracteres);
     });
@@ -29,27 +30,44 @@ function inicializaContadores() {
 
 function inicializaCronometro() {
     var tempoRestante = $("#tempo-digitacao").text();
-    campo.one("focus",function(){
-        var cronometroID = setInterval(function(){
+    campo.one("focus", function () {
+        var cronometroID = setInterval(function () {
             tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
-            if(tempoRestante < 1){
-                campo.attr("disabled",true);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
                 clearInterval(cronometroID);
                 campo.toggleClass("campo-desativado");
-            }    
-        },1000);
+            }
+        }, 1000);
     })
 }
 
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function () {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length);
+        if (digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+}
+
 function reiniciaJogo() {
-    campo.attr("disabled",false);
+    campo.attr("disabled", false);
     campo.val("");
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
     campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
 }
 
 
