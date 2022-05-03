01c7a4afce0ef4878275c73082acfd2af52ff26e
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-05-03 17:01:59 -0300

    feat: add new table for users and their scores

--- public/js/main.js
+++ public/js/main.js
@@ -35,13 +35,19 @@ function inicializaCronometro() {
             tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
             if (tempoRestante < 1) {
-                campo.attr("disabled", true);
                 clearInterval(cronometroID);
-                campo.toggleClass("campo-desativado");
+                finalizaJogo();
             }
         }, 1000);
     })
 }
+
+function finalizaJogo() {
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
+
 
 function inicializaMarcadores() {
     var frase = $(".frase").text();
@@ -56,6 +62,19 @@ function inicializaMarcadores() {
             campo.removeClass("borda-verde");
         }
     });
+}
+
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Thaís";
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = "<tr>" +
+        "<td>" + usuario + "</td>" +
+        "<td>" + numPalavras + "</td>" +
+        "</tr>";
+
+    corpoTabela.prepend(linha);
 }
 
 function reiniciaJogo() {
