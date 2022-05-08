b9beda594b7f20805c94e1a9896fb74b015e12e9
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-05-08 09:35:14 -0300

    feat: add select users

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,10 @@ $(document).ready(function () {
     inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
     atualizaPlacar();
+    $("#usuarios").selectize({
+        create: true,
+        sortField: 'text',
+    });
 });
 
 function atualizaTempoInicial(tempo) {
--- public/js/placar.js
+++ public/js/placar.js
@@ -3,7 +3,7 @@ $("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Thaís";
+    var usuario = $("#usuarios").val();
     var numPalavras = $("#contador-palavras").text();
 
     var linha = novaLinha(usuario, numPalavras);
@@ -14,12 +14,12 @@ function inserePlacar() {
     scrollPlacar();
 }
 
-function scrollPlacar(){
+function scrollPlacar() {
     var posicaoPlacar = $(".placar").offset().top;
     $("html, body").animate(
-    {
-        scrollTop: posicaoPlacar+"px"
-    },1000);
+        {
+            scrollTop: posicaoPlacar + "px"
+        }, 1000);
 }
 
 function novaLinha(usuario, palavras) {
@@ -28,7 +28,7 @@ function novaLinha(usuario, palavras) {
     var colunaPalavras = $("<td>").text(palavras);
     var colunaRemover = $("<td>");
 
-    var link = $("<a>").addClass("botao-remover").attr("href","#");
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
     var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
 
     link.append(icone);
@@ -43,12 +43,12 @@ function novaLinha(usuario, palavras) {
 }
 
 function removeLinha() {
-        event.preventDefault();
-        var linha = $(this).parent().parent();
-        linha.fadeOut(1000);
-        setTimeout(function(){
-            linha.remove();
-        },1000)
+    event.preventDefault();
+    var linha = $(this).parent().parent();
+    linha.fadeOut(1000);
+    setTimeout(function () {
+        linha.remove();
+    }, 1000)
 }
 
 function mostraPlacar() {
@@ -58,10 +58,10 @@ function mostraPlacar() {
 function sincronizaPlacar() {
     var placar = [];
     var linhas = $("tbody>tr");
-    linhas.each(function() {
+    linhas.each(function () {
         var usuario = $(this).find("td:nth-child(1)").text();
         var palavras = $(this).find("td:nth-child(2)").text();
-        
+
         var score = {
             usuario: usuario,
             pontos: palavras
@@ -74,14 +74,15 @@ function sincronizaPlacar() {
         placar: placar
     };
 
-    $.post("http://localhost:3000/placar", dados, function(){
+    $.post("http://localhost:3000/placar", dados, function () {
     })
 }
 
 function atualizaPlacar() {
-    $.get("http://localhost:3000/placar", function(data){
-        $(data).each(function(){
+    $.get("http://localhost:3000/placar", function (data) {
+        $(data).each(function () {
             var linha = novaLinha(this.usuario, this.pontos);
+            linha.find(".botao-remover").click(removeLinha);
             $("tbody").append(linha);
         })
     })
